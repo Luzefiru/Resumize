82ac6c17d6def0cf51e30d5cf11b7290e5296558
Luzefiru <ian.deejay.iv@gmail.com>
2023-03-23 19:00:52 +0800

    feat: finalize Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Logo from '../res/logo-combination-white.svg';
+import '../styles/Header.css';
+
+class Header extends React.Component {
+  render() {
+    return (
+      <header className="Header">
+        <img className="Header__logo" src={Logo} alt="Logo"></img>
+      </header>
+    );
+  }
+}
+
+export default Header;
