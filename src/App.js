06e57dd5b5cb32928158e75edc49290363a13ad8
Luzefiru <ian.deejay.iv@gmail.com>
2023-03-23 12:25:22 +0800

    feat: add functionality to add tasks as a state in App

--- src/App.js
+++ src/App.js
@@ -1,8 +1,38 @@
 import React from 'react';
+import Overview from './components/Overview';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tasks: [],
+    };
+    this.addTask = this.addTask.bind(this);
+  }
+  async addTask() {
+    const name = document.querySelector('#taskField').value;
+    await this.setState((state, props) => ({
+      tasks: state.tasks.concat({ name }),
+    }));
+    document.querySelector('#taskField').value = '';
+  }
   render() {
-    return <div>Hello World!</div>;
+    return (
+      <div>
+        <h3>Add a Task:</h3>
+        <form action="#">
+          <label for="taskField">Task Name: </label>
+          <input type="text" placeholder="Task Name" id="taskField"></input>
+          <button type="submit" onClick={this.addTask}>
+            Add
+          </button>
+        </form>
+        <br />
+        <br />
+        <h3>Current Tasks:</h3>
+        <Overview tasks={this.state.tasks} />
+      </div>
+    );
   }
 }
 
