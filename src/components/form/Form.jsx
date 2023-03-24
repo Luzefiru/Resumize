746fead3c1ce897bc171e7a9e5d86beb8cc1c381
Luzefiru <ian.deejay.iv@gmail.com>
2023-03-24 09:57:06 +0800

    feat: move form--header form into its own component

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -1,58 +1,26 @@
 import React from 'react';
 import './Form.css';
+import FormHeader from './FormHeader';
 
 class Form extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.handleSubmitHeaderDetails = this.handleSubmitHeaderDetails.bind(this);
+  }
+
+  handleSubmitHeaderDetails() {
+    document.querySelector('.form--header').classList.toggle('hide');
+  }
+
   render() {
     return (
       <div className="Form">
-        <form className="form--heading">
-          <fieldset>
-            <legend>Header Details</legend>
-            <label htmlFor="fName">First Name: </label>
-            <input
-              onChange={this.props.headerDetailsFunctions.handleChangeFName}
-              type="text"
-              name="fName"
-              id="fName"
-              value={this.props.headerDetails.fName}
-            />
-            <label htmlFor="lName">Last Name: </label>
-            <input
-              onChange={this.props.headerDetailsFunctions.handleChangeLName}
-              type="text"
-              name="lName"
-              id="lName"
-              value={this.props.headerDetails.lName}
-            />
-            <label htmlFor="email">Email Address: </label>
-            <input
-              onChange={this.props.headerDetailsFunctions.handleChangeEmail}
-              type="email"
-              name="email"
-              id="email"
-              value={this.props.headerDetails.email}
-            />
-            <label htmlFor="phone">Phone Number: </label>
-            <input
-              onChange={this.props.headerDetailsFunctions.handleChangeTelNumber}
-              type="tel"
-              name="phone"
-              id="phone"
-              value={this.props.headerDetails.telNumber}
-            />
-            <label htmlFor="location">Location: </label>
-            <input
-              onChange={this.props.headerDetailsFunctions.handleChangeLocation}
-              type="text"
-              name="location"
-              id="location"
-              value={this.props.headerDetails.location}
-            />
-            {/* <button type="button" onClick={this.handleSubmitHeaderDetails}>
-              Submit
-            </button> */}
-          </fieldset>
-        </form>
+        <FormHeader
+          headerDetails={this.props.headerDetails}
+          headerDetailsFunctions={this.props.headerDetailsFunctions}
+          handleSubmitHeaderDetails={this.handleSubmitHeaderDetails}
+        />
       </div>
     );
   }
---
+++ src/components/form/FormHeader.jsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import './FormHeader.css';
+
+class FormHeader extends React.Component {
+  render() {
+    return (
+      <form className="form--header">
+        <fieldset>
+          <legend>Header Details</legend>
+          <label htmlFor="fName">First Name: </label>
+          <input
+            onChange={this.props.headerDetailsFunctions.handleChangeFName}
+            type="text"
+            name="fName"
+            id="fName"
+            value={this.props.headerDetails.fName}
+          />
+          <label htmlFor="lName">Last Name: </label>
+          <input
+            onChange={this.props.headerDetailsFunctions.handleChangeLName}
+            type="text"
+            name="lName"
+            id="lName"
+            value={this.props.headerDetails.lName}
+          />
+          <label htmlFor="email">Email Address: </label>
+          <input
+            onChange={this.props.headerDetailsFunctions.handleChangeEmail}
+            type="email"
+            name="email"
+            id="email"
+            value={this.props.headerDetails.email}
+          />
+          <label htmlFor="phone">Phone Number: </label>
+          <input
+            onChange={this.props.headerDetailsFunctions.handleChangeTelNumber}
+            type="tel"
+            name="phone"
+            id="phone"
+            value={this.props.headerDetails.telNumber}
+          />
+          <label htmlFor="location">Location: </label>
+          <input
+            onChange={this.props.headerDetailsFunctions.handleChangeLocation}
+            type="text"
+            name="location"
+            id="location"
+            value={this.props.headerDetails.location}
+          />
+          <button type="button" onClick={this.props.handleSubmitHeaderDetails}>
+            Submit
+          </button>
+        </fieldset>
+      </form>
+    );
+  }
+}
+
+export default FormHeader;
