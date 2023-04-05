c18c60befb4424090ac16bedbbdef8b2562ff5af
Luzefiru <ian.deejay.iv@gmail.com>
2023-04-05 14:49:34 +0800

    feat: confirm everything is working

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -2,12 +2,27 @@ import React from 'react';
 import Logo from '../../res/logo-combination-white.svg';
 import './Header.css';
 
+import { auth } from '../../firebase-config';
+import { signOut } from 'firebase/auth';
+
+const logOut = () => {
+  signOut(auth)
+    .then(() => {
+      console.log("You've signed out.");
+    })
+    .catch((e) => {
+      console.log('Failed to sign out:', e);
+    });
+};
+
 class Header extends React.Component {
   render() {
     return (
       <header className="Header">
         <img className="Header__logo" src={Logo} alt="Logo"></img>
-        <div className="Header__logout">Log Out</div>
+        <div onClick={logOut} className="Header__logout">
+          Log Out
+        </div>
       </header>
     );
   }
